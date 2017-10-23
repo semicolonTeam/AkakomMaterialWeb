fe1cf8604a88fdfb55875abf3bc9868a7a295017
Arief Permana <permanaarief8@gmail.com>
2017-10-23 16:26:45 +0700

    (fix) Routing

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ app.get('/',(req,res)=>{
 	res.sendFile(page.getPath('home'))
 })
 
+app.get('/berita',(req,res)=>{
+	res.sendFile(page.getPath('berita'))
+})
+
+app.get('/info',(req,res)=>{
+	res.sendFile(page.getPath('info'))
+})
+
+app.get('/sejarah',(req,res)=>{
+	res.sendFile(page.getPath('sejarah'))
+})
+
+app.get('/tautan',(req,res)=>{
+	res.sendFile(page.getPath('tautan'))
+})
+
+app.get('*',(req,res)=>{
+	res.redirect('/')
+})
+
+
 app.listen(port, ()=>{
 	console.log(`Akakom Material Design berjalan pada http://localhost:${port}/`)
 })
